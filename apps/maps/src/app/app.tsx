f8c51912969d598c171ed85c09a5eeb24f636b38
Kjartan Bjørset <kjabjo@norkart.no>
2022-01-20 00:49:18 +0100

    wms

--- apps/maps/src/app/app.tsx
+++ apps/maps/src/app/app.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';  
-import { FeatureGroup, GeoJson, MapContainer, Marker, Popup, TileLayer } from 'react-map-leaflet';  
+import { LatLngBounds } from 'leaflet';
+import { FeatureGroup, GeoJson, MapContainer, Marker, Popup, TileLayer, TileLayerWms } from 'react-map-leaflet';  
 
 const StyledApp = styled.div`
   // Your style here
@@ -9,11 +10,20 @@ export function App() {
   return (
     <StyledApp>
 
-    <MapContainer options={{ zoom: 14, center: [50,10]}}> 
-    <FeatureGroup>   
-        <Marker onClick={(event) => {console.log("click marker", event);}} latlng={[50,10]} options={{ }}> </Marker> 
-    </FeatureGroup> 
+    <MapContainer options={{ zoom: 14, center: [63.3,10.3]}}> 
+        <FeatureGroup>   
+            <Marker onClick={(event) => {console.log("click marker", event);}} latlng={[50,10]} options={{ }}> </Marker> 
+        </FeatureGroup> 
         <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' maxZoom={18} minZoom={0} ></TileLayer>
+        
+        <TileLayerWms url={"http://kart.trondheim.kommune.no/geoserver/Raster/wms?"} 
+            maxZoom={18} 
+            minZoom={12} 
+            format={"image/png"} 
+            layers='DTM_Skyggemodell' 
+            transparent={true} 
+            opacity={0.5}
+            bounds={new LatLngBounds([63.298322940154904, 10.04545530381227],[63.46114177810909, 10.740909239294403])}/>
     </MapContainer>  
 
       </StyledApp>
